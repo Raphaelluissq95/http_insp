d56375ee34cdbcbd4f22134ba461fb252c90d91e
Felipe Augusto Rodrigues Brandão <felipe.arbrandao@gmail.com>
2018-07-07 20:39:05 -0300

    Validação da chamada do aplicativo
    Chamada deve ter 3 argumentos ou 1 (próprio nome do binário) e aceita como única opção [-p <port>[=8228]]

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,35 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include "Server.h"
 
 #define PORT_NUM 8228
 
-int main() {
-	int port = PORT_NUM;
-	printf("Iniciando conexão com porta #%d\n",port);
+int main(int argc, char * const argv[]) {
+	long port;
 
-	Server server(port);
+	if (argc != 1) {
+        if (argc != 3) {
+            printf("\nO aplicativo deve ser usado da seguinte forma:\n# ./aracne [-p <port>[=8228]]\n");
+            exit(EXIT_FAILURE);
+        } else if (strcmp(argv[1], "-p") != 0) {
+            printf("\nA unica opcao possivel eh [-p <port>[=8228]]\n");
+            exit(EXIT_FAILURE);
+        }
+
+        port = strtol(argv[2], nullptr, 10); // convertendo a porta de string para long
+
+        if (port < 1024 || port > 65535) {
+            printf("\nO numero da porta deve estar no range valido de 1024 a 65535\n");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+	    port = PORT_NUM;
+	}
+
+    printf("Iniciando conexão com porta #%ld\n", port);
+
+	Server server((int) port);
 
 	while(server.Init());
 
